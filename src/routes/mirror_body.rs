5eedf946285fff8124ab20069a04706bcbfe956a
Tuan Vu Hoang Quang <tuanvu.hoangquang@proton.me>
2025-09-22 23:35:08 +0200

    feat: add path variables handler

--- src/routes/mirror_body.rs
+++ src/routes/mirror_body.rs
@@ -1,7 +1,7 @@
 use axum::Json;
 use serde::{Deserialize, Serialize};
 
-pub async fn mirror_body_string(body: String) -> String {
+pub async fn mirror_body_string_handler(body: String) -> String {
     body
 }
 
@@ -15,7 +15,9 @@ pub struct MirrorJsonResponse {
     message: String,
 }
 
-pub async fn mirror_body_json(Json(body): Json<MirrorJsonBody>) -> Json<MirrorJsonResponse> {
+pub async fn mirror_body_json_handler(
+    Json(body): Json<MirrorJsonBody>,
+) -> Json<MirrorJsonResponse> {
     Json(MirrorJsonResponse {
         message: body.message,
     })
--- src/routes/mod.rs
+++ src/routes/mod.rs
@@ -1,16 +1,22 @@
 mod hello_world;
 mod mirror_body;
+mod path_variables;
 
 use axum::{
     Router,
     routing::{get, post},
 };
 use hello_world::hello_world;
-use mirror_body::{mirror_body_json, mirror_body_string};
+
+use crate::routes::{
+    mirror_body::{mirror_body_json_handler, mirror_body_string_handler},
+    path_variables::path_variables_handler,
+};
 
 pub fn create_routes() -> Router {
     Router::new()
         .route("/", get(hello_world))
-        .route("/mirror_body_string", post(mirror_body_string))
-        .route("/mirror_body_json", post(mirror_body_json))
+        .route("/mirror_body_string", post(mirror_body_string_handler))
+        .route("/mirror_body_json", post(mirror_body_json_handler))
+        .route("/path_variables/{id}", get(path_variables_handler))
 }
